f2d172912be0f61a874c8ed552c440e255a4fbfd
Jon Galloway <jon@jongalloway.design>
2022-11-24 20:16:19 -0600

    Practice Problems for 11-24-22

--- index.js
+++ index.js
@@ -81,13 +81,9 @@ function lovefunc(flower1, flower2) {
 
 
 // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-
 // The output should be two capital letters with a dot separating them.
-
 // It should look like this:
-
 // Sam Harris => S.H
-
 // patrick feeney => P.F
 
 function abbrevName(name){
@@ -146,3 +142,52 @@ function points(games) {
 };
 
 
+
+
+//Roman Numeral to Int
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+function romanToInt(romanNumeral) {
+
+  let numberConverter = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+
+  let total = 0;
+
+  for(let i=0; i<romanNumeral.length; i++) {
+
+    let currentPosition = numberConverter[romanNumeral.charAt(i)];
+    let nextPosition = numberConverter[romanNumeral.charAt(i + 1)]
+    if(nextPosition) {
+      if(currentPosition >= nextPosition) {
+        total += currentPosition;
+        } else {
+          total += (nextPosition - currentPosition)
+          i++
+        }
+    } else {
+      total += currentPosition;
+    }
+
+  }
+  console.log(total);
+}
+
+
+romanToInt("MCMXCVI");
